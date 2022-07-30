ac71cb224f7d84cea7b7f4429725aa40956217c0
Tonyeli Tay <taytonyeli@gmail.com>
2022-07-30 13:55:05 +0000

    feat: switch from preact to gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,41 @@
+module.exports = {
+  siteMetadata: {
+    title: `Quick Intern`,
+    description: `Quick Intern: Find internships, jobs in Ghana`,
+    author: `@taytonyeli`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-image`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-sass`,
+      options: {
+        sassOptions: {
+          precision: 6,
+        },
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `quick-intern`,
+        short_name: `quick-intern`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `standalone`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    `gatsby-plugin-gatsby-cloud`,
+  ],
+}
--- src/components/form/FormButton.js
+++
@@ -1,7 +0,0 @@
-import { h } from 'preact'
-
-const FormButton = ({ title }) => {
-  return <button className='formButton'>{title}</button>
-}
-
-export default FormButton
